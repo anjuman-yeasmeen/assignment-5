e7276b3dd409aa2300d63b06b1e698f6794b73ea
anjuman-yeasmeen <malarashid14@gmail.com>
2025-08-29 16:48:40 +0600

    copy functionality implemented

--- script.js
+++ script.js
@@ -13,3 +13,24 @@ heartIcons.forEach((hearticon) => {
     targetedElement.classList.add("text-red-400", "fa-solid");
   });
 });
+
+// copy
+const copyIcons = document.querySelectorAll(".copy-btn");
+
+copyIcons.forEach((copybtn) => {
+  copybtn.addEventListener("click", function (event) {
+    event.preventDefault();
+    console.log(event.target);
+
+    const copyCountElement = document.getElementById("copy-count");
+    let copyCount = parseInt(copyCountElement.innerText);
+    copyCount = copyCount + 1;
+    copyCountElement.innerText = copyCount;
+    alert("Hotline number hasbeen copied");
+
+    const hotlineNumber =
+      event.target.parentNode.parentNode.querySelector("#hotline-number").innerText;
+
+    navigator.clipboard.writeText(hotlineNumber);
+  });
+});
